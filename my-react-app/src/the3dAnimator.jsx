7424d866af2e2e4a39b62c2cd24ae3c76587e269
Trong Long Tran <09longtr09@gmail.com>
2025-12-27 18:01:51 -0800

    first commit

---
+++ my-react-app/src/the3dAnimator.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Canvas, useFrame } from "@react-three/fiber"
+import { useRef } from "react";
+import * as THREE from 'three'
+import { useGLTF } from '@react-three/drei'
