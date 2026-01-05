65712bba05dd36c53dec671aa7af12a5e6f90878
Trong Long Tran <09longtr09@gmail.com>
2026-01-04 20:31:50 -0800

    add font

--- wangara/src/main.jsx
+++ wangara/src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+// Supports weights 400-700
+import '@fontsource-variable/lora';
+import '@fontsource/source-serif-pro';
+// Supports weights 100-700
+import '@fontsource-variable/roboto-mono';
+// Supports weights 400-900
+import '@fontsource-variable/ruda';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
